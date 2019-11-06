d2be3605fc7edf2f5ec976dd3e5267d8a601981f
Ameya Vikram Singh <ameya.v.singh@gmail.com>
2019-11-06 21:01:24 +0530

    Example for redirect

--- Python/flask_tutorial/tutorial01.py
+++ Python/flask_tutorial/tutorial01.py
@@ -1,11 +1,25 @@
 from flask import Flask
+from flask import redirect
+from flask import url_for
 
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
     return "Hello this is the main page <h1>HELLO</h1>"
+
+
+@app.route("/<name>")
+def user(name):
+    return f"Hello {name}!"
+
+
+@app.route("/admin")
+def admin():
+    return redirect(url_for("home"))
+
 
 if __name__ == "__main__":
     app.run()
