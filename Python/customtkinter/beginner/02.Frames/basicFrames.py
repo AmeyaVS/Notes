8ed73cae9d6051f74c52f511997f85936ac683f7
Ameya Vikram Singh <ameya.v.singh@gmail.com>
2024-02-26 21:07:32 +0530

    Add a basic UI Frame

--- Python/customtkinter/beginner/02.Frames/basicFrames.py
+++ Python/customtkinter/beginner/02.Frames/basicFrames.py
@@ -11,9 +11,16 @@ class App(customtkinter.CTk):
         self.grid_columnconfigure(0, weight=1)
         self.grid_rowconfigure((0, 1), weight=1)
 
-        self.checkbox_1 = customtkinter.CTkCheckBox(self, text="checkbox_1")
+        self.checkbox_frame = customtkinter.CTkFrame(self)
+        self.checkbox_frame.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="nsw")
+
+        self.checkbox_1 = customtkinter.CTkCheckBox(
+            self.checkbox_frame, text="checkbox 1"
+        )
         self.checkbox_1.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="w")
-        self.checkbox_2 = customtkinter.CTkCheckBox(self, text="checkbox_2")
+        self.checkbox_2 = customtkinter.CTkCheckBox(
+            self.checkbox_frame, text="checkbox 2"
+        )
         self.checkbox_2.grid(row=1, column=0, padx=10, pady=(10, 0), sticky="w")
         self.button = customtkinter.CTkButton(
             self, text="my button", command=self.button_callback
