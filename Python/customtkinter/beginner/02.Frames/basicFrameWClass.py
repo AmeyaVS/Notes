e33b9bb4761d0b5dd54bed8ede9af2c6fb5c51df
Ameya Vikram Singh <ameya.v.singh@gmail.com>
2024-02-26 21:10:44 +0530

    Add another checkbox in the frame

--- Python/customtkinter/beginner/02.Frames/basicFrameWClass.py
+++ Python/customtkinter/beginner/02.Frames/basicFrameWClass.py
@@ -9,6 +9,8 @@ class MyCheckboxFrame(customtkinter.CTkFrame):
         self.checkbox_1.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="w")
         self.checkbox_2 = customtkinter.CTkCheckBox(self, text="checkbox 2")
         self.checkbox_2.grid(row=1, column=0, padx=10, pady=(10, 0), sticky="w")
+        self.checkbox_3 = customtkinter.CTkCheckBox(self, text="checkbox 3")
+        self.checkbox_3.grid(row=2, column=0, padx=10, pady=(10, 0), sticky="w")
 
 
 class App(customtkinter.CTk):
