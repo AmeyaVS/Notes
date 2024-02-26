83ee8f5a07951b25fe6570b8ebd67f8b10814aa5
Ameya Vikram Singh <ameya.v.singh@gmail.com>
2024-02-26 21:22:05 +0530

    Add a basic Example of Dynamic Frame Elements

---
+++ Python/customtkinter/beginner/02.Frames/basicDynamicFrameClass.py
@@ -0,0 +1,47 @@
+import customtkinter
+
+
+class MyCheckboxFrame(customtkinter.CTkFrame):
+    def __init__(self, master, values):
+        super().__init__(master)
+
+        self.values = values
+        self.checkboxes = []
+
+        for i, value in enumerate(self.values):
+            checkbox = customtkinter.CTkCheckBox(self, text=value)
+            checkbox.grid(row=i, column=0, padx=10, pady=(10, 0), sticky="w")
+            self.checkboxes.append(checkbox)
+
+    def get(self):
+        checked_checkboxes = []
+        for checkbox in self.checkboxes:
+            if checkbox.get():
+                checked_checkboxes.append(checkbox.cget("text"))
+        return checked_checkboxes
+
+
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+
+        self.title("my app")
+        self.geometry("400x180")
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_rowconfigure(0, weight=1)
+
+        self.checkbox_frame = MyCheckboxFrame(self, values=["value 1", "value 2", "value 3"])
+        self.checkbox_frame.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="nsw")
+
+        self.button = customtkinter.CTkButton(
+            self, text="my button", command=self.button_callback
+        )
+        self.button.grid(row=3, column=0, padx=10, pady=10, sticky="ew")
+
+    def button_callback(self):
+        print("button pressed")
+        print("checked checkboxes: ", self.checkbox_frame.get())
+
+
+app = App()
+app.mainloop()
