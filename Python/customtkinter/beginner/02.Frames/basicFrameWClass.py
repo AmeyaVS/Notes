9eeff3cbb4cbcb95548075b2e61526c536998bec
Ameya Vikram Singh <ameya.v.singh@gmail.com>
2024-02-26 21:15:20 +0530

    Extending Frame Class

--- Python/customtkinter/beginner/02.Frames/basicFrameWClass.py
+++ Python/customtkinter/beginner/02.Frames/basicFrameWClass.py
@@ -12,6 +12,16 @@ class MyCheckboxFrame(customtkinter.CTkFrame):
         self.checkbox_3 = customtkinter.CTkCheckBox(self, text="checkbox 3")
         self.checkbox_3.grid(row=2, column=0, padx=10, pady=(10, 0), sticky="w")
 
+    def get(self):
+        checked_checkboxes = []
+        if self.checkbox_1.get():
+            checked_checkboxes.append(self.checkbox_1.cget("text"))
+        if self.checkbox_2.get():
+            checked_checkboxes.append(self.checkbox_2.cget("text"))
+        if self.checkbox_3.get():
+            checked_checkboxes.append(self.checkbox_3.cget("text"))
+        return checked_checkboxes
+
 
 class App(customtkinter.CTk):
     def __init__(self):
@@ -32,6 +42,7 @@ class App(customtkinter.CTk):
 
     def button_callback(self):
         print("button pressed")
+        print("checked checkboxes: ", self.checkbox_frame.get())
 
 
 app = App()
